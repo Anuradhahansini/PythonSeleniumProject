adb07e70d89d6fc38431e7629ed2f979470e48df
AnuradhaHansini <hansinianuradha1116@gmail.com>
2025-07-23 13:54:54 +0530

    basic things operators

---
+++ OperatorsDemo.py
@@ -0,0 +1,55 @@
+#arithmatic Operators +,_,*,/,//,%,**
+a= 12
+b=10
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b) # round of number
+print(a%b)  # remind value
+print(2**2) # power by number
+#Comparison Operators >,<,=, !=,>=,<=
+a= 12
+b=10
+print(a<b)
+print(a>b)
+print(a==b)
+print(a!=b)
+print(a<=b)
+print(a>=b)
+#Logical Operators  and,or,not
+bool1=True
+bool2=False
+print(bool1 and bool2)
+print(bool1 or bool2)
+print(not bool2)  #opersit
+
+
+
+#Assigment Operators  =,+=,-=,*=,/=,%=,//=
+a=10
+a = a+5
+print(a)
+a +=5
+print(a)
+a*=5
+print(a)
+a/=5
+print(a)
+a%=5
+print(a)
+a//=5
+print(a)
+#Identity Operators is ,is not
+bool1=True
+bool2=False
+
+print (bool1 is not bool2)
+
+
+#Membership Operators in, not in
+li =[1,3,4,6]
+
+print(5 in li)
+
+print(5 not in li)
